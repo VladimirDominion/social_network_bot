7ccc981c03db6b8c8f855dfaaebe02c80be5a8d0
vladimir <webmagic@ukr.net>
2021-04-26 13:26:00 +0300

    clean files

--- README.md
+++ README.md
@@ -7,3 +7,10 @@ python main.py
 ## For test
 
 pytest
+
+## TODO
+
+1. Create task in Classes
+2. Add error heandlers
+3. Add Synchronization Primitives
+4. etc ...
--- tasks/base_task.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class BaseTask(ABC):
-    @abstractmethod
-    def get_url(self):
-        """ Should return url for task """
